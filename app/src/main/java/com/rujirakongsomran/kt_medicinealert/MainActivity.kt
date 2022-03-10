e922f6ae846373e925563834e09c8b31571c0225
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-03-10 17:39:59 +0700

    Bottom navigation with FAB finish

--- app/src/main/java/com/rujirakongsomran/kt_medicinealert/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_medicinealert/MainActivity.kt
@@ -2,10 +2,20 @@ package com.rujirakongsomran.kt_medicinealert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rujirakongsomran.kt_medicinealert.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.bnv.background = null
+        binding.bnv.menu.getItem(2).isEnabled = false
+
+
     }
 }
